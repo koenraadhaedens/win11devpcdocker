5ec233395bcc77d1876410b3d2f96ee1ab9eb5e7
koenraadhaedens <40601026+koenraadhaedens@users.noreply.github.com>
2022-12-09 10:53:18 +0100

    Update devpc.ps1

--- devpc.ps1
+++ devpc.ps1
@@ -3,7 +3,7 @@ $UserName = $args[0]
 Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Windows-Subsystem-Linux
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
 Invoke-WebRequest https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile c:\\wsl_update_x64.msi -UseBasicParsing
-Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile c:\\ubuntu2004.appx -UseBasicParsing
+Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile c:\\ubuntu2004.appx
 
 
 
