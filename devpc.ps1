94682c83a53dea03878d7916bccc85e625deef4e
Koenraad Haedens <40601026+koenraadhaedens@users.noreply.github.com>
2023-10-13 09:22:29 +0200

    Update devpc.ps1

--- devpc.ps1
+++ devpc.ps1
@@ -19,7 +19,7 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRes
 Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install docker-desktop git vscode azure-cli kubernetes-helm kubernetes-cli vscode-kubernetes-tools -y
+choco install python docker-desktop git vscode azure-cli kubernetes-helm kubernetes-cli vscode-kubernetes-tools -y
 
 
 
